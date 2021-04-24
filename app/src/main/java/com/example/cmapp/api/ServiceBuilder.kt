54bbf5235a9bfe425cb4b5bb38a7d3712b07d887
PalomaWolfango <paloma.wolfango@gmail.com>
2021-04-24 20:27:28 +0100

    teste

--- app/src/main/java/com/example/cmapp/api/ServiceBuilder.kt
+++ app/src/main/java/com/example/cmapp/api/ServiceBuilder.kt
@@ -9,7 +9,7 @@ object ServiceBuilder {
     private val client = OkHttpClient.Builder().build()
 
     private val retrofit = Retrofit.Builder()
-        .baseUrl("https://smartcitycmei.000webhostapp.com/myslim/")
+        .baseUrl("https://tp1cm.000webhostapp.com/myslim/")
         .addConverterFactory(GsonConverterFactory.create())
         .client(client)
         .build()
